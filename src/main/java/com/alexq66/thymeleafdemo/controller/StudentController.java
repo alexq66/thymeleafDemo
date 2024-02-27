2ebdf2406490f0cb2be7bfc8b461604e94db732c
Kenneth Quinn <kqui108@wgu.edu>
2024-02-27 12:02:39 -0600

    Add student confirmation template and process method
    Added a new HTML template, `student-confirmation.html`, for confirming student registration. Updated `StudentController` to include a method for processing the form data. The form displays the full name of the newly registered student.

--- src/main/java/com/alexq66/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/alexq66/thymeleafdemo/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.alexq66.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by Kenneth Quinn on 2/27/2024
@@ -18,5 +20,11 @@ public class StudentController {
         theModel.addAttribute("student", new Student());
 
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+        System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        return "student-confirmation";
     }
 }
