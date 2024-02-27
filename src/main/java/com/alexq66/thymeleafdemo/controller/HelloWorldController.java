079356fe1bde6ed23355948fe712f0141c547923
Kenneth Quinn <kqui108@wgu.edu>
2024-02-27 10:56:41 -0600

    Add uppercase conversion functionality to form processing
    A new functionality is introduced to the form processing in the HelloWorldController, transforming the inputted student's name into uppercase. The corresponding HTML templates were also updated to accommodate this change, with the addition of a new field to display the converted name.

--- src/main/java/com/alexq66/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/alexq66/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.alexq66.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,6 +20,15 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String upperCaseName(HttpServletRequest request, Model model) {
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "Welcome! " + theName;
+        model.addAttribute("message", result);
         return "helloworld";
     }
 
