644593ab640a103ee1eab12360f249f9e46d4877
Kenneth Quinn <kqui108@wgu.edu>
2024-02-27 11:18:04 -0600

    Update form processing to convert student's name to uppercase
    A new method named `upperCaseNameTwo` is introduced to the `HelloWorldController`. It processes the name entered on the form by converting it to uppercase before displaying the message. Along with this, the form action in the HTML template is modified to call the new method.

--- src/main/java/com/alexq66/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/alexq66/thymeleafdemo/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Kenneth Quinn on 2/27/2024
@@ -26,6 +27,14 @@ public class HelloWorldController {
     @RequestMapping("/processFormVersionTwo")
     public String upperCaseName(HttpServletRequest request, Model model) {
         String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "Welcome! " + theName;
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String upperCaseNameTwo(@RequestParam("studentName") String theName, Model model) {
         theName = theName.toUpperCase();
         String result = "Welcome! " + theName;
         model.addAttribute("message", result);
