32a21f5cc9bc8c4643e235e36af04081aa34d3f1
Kenneth Quinn <kqui108@wgu.edu>
2024-02-27 11:49:14 -0600

    Add new Student model class
    The Student class has been added to the 'model' package to hold student's information, featuring 'firstName' and 'lastName' fields. It includes getter and setter methods for these fields, facilitating manipulation of student data within the application.

---
+++ src/main/java/com/alexq66/thymeleafdemo/model/Student.java
@@ -0,0 +1,30 @@
+package com.alexq66.thymeleafdemo.model;
+
+/**
+ * Created by Kenneth Quinn on 2/27/2024
+ */
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    public Student() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
