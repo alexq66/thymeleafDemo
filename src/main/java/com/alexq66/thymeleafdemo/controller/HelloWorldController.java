02ef51309fc2ce06fad4e5783f975f7554fa0146
Kenneth Quinn <kqui108@wgu.edu>
2024-02-27 10:34:26 -0600

    Add HelloWorldController and helloworld-form.html
    This commit includes the creation of HelloWorldController in our application providing the "showForm" method. In addition,

---
+++ src/main/java/com/alexq66/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.alexq66.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Kenneth Quinn on 2/27/2024
+ */
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+}
