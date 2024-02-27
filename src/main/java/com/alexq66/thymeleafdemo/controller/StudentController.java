25f93c2b6a9022161f21f10b523191b4b8f3e9aa
Kenneth Quinn <kqui108@wgu.edu>
2024-02-27 13:21:08 -0600

    Update application properties and StudentController for country list
    Updated application.properties to include a list of countries. Modified StudentController to extract this list and pass it to student form. Also, student-form.html has been adjusted to dynamically generate the country selection options based on the provided list.

--- src/main/java/com/alexq66/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/alexq66/thymeleafdemo/controller/StudentController.java
@@ -1,11 +1,14 @@
 package com.alexq66.thymeleafdemo.controller;
 
 import com.alexq66.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /**
  * Created by Kenneth Quinn on 2/27/2024
@@ -14,10 +17,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class StudentController {
 
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
         theModel.addAttribute("student", new Student());
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
